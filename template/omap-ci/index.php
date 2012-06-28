8baea843b40ba729487e0d681ba6a1a26834a58e
Agus Prasetyo <agusprasetyo811@gmail.com>
2012-06-28 16:57:09 +0700

    Update Template Interface

--- template/omap-ci/index.php
+++ template/omap-ci/index.php
@@ -1,15 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="{STYLE}style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>{TITLE}</title>
+<link href="{STYLE}style.css" rel="stylesheet" type="text/css" />
 </head>
-<body>
-	<div id="layout">
-		<div id="header"></div>
-		<div id="body"><img src="{IMAGES}icon.png"/></div>
-		<div id="footer"></div>
-	</div>
+<body class="bg_all">
+<div id="layout" class="all">
+    <div id="search">
+    </div>
+    <div id="body">
+    	<div id="header"><div align="center" valign="middle"> == HEADER == </div></div>
+        <!-- Top Menus -->
+        <div id="menus"></div>
+        <!-- Content -->
+        <div id="content">
+            <table border="0">
+            	<tr valign="top">
+                	<td>
+                    	<div id="content-left"> 
+                    		<div align="center">== SIDE LEFT ==</div>
+                        </div>
+                    </td>
+                    <td>
+                    	<div id="content-center">
+                    		{BODY_CENTER}
+                        </div>
+                    </td>
+                    <td>
+                    	<div id="content-right">
+                    		<div align="center">== SIDE RIGHT ==</div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <!-- Footer -->
+        <div id="footer">{AUTHOR_SITE}</div>
+    </div>
+</div>
+<br /><br />
 </body>
 </html>
