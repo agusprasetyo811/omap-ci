14d694af64f7c74f1ac57084a4916364a7d1954b
Agus Prasetyo <agusprasetyo811@gmail.com>
2013-01-03 08:58:18 +0700

    add param site_index

--- application/libraries/omap.php
+++ application/libraries/omap.php
@@ -208,7 +208,8 @@ class Omap {
 		$file_data['STYLE'] = base_url().'template/'.$new_template.'/style/';
 		$file_data['JS'] = base_url().'template/'.$new_template.'/js/';
 		$file_data['IMAGES'] = IMG_PATH;base_url().'template/'.$new_template.'/images/'
-		$file_data['SITE'] = base_url().'index.php/';
+		$file_data['SITE_INDEX'] = base_url().'index.php/';
+		$file_data['SITE'] = base_url();
 		$file_data['AUTHOR'] = '&copy '.date('Y').' omap-ci - omap. All Right Reserved';
 		$file_data['DEVELOPER'] = '<a href="http://github.com/agusprasetyo811/omap-ci/">Developer</a>';
 		$file_data['DEVELOPER_SITE'] = '<a href="http://cmlocator.com/">Website</a>';
