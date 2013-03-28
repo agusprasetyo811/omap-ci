38670fc775f3d32a982a7c450c465052cee93b8c
Agus Prasetyo <agusprasetyo811@gmail.com>
2013-03-28 11:31:52 +0700

    update new config

--- system/core/Config.php
+++ system/core/Config.php
@@ -112,10 +112,10 @@ class CI_Config {
 		define('IMAGES', TEMPLATE.'/images/');
 		
 		// Define site  PATH (omap-ci)
-		define('SITE', $base_url);
+		define('SITE', base_url());
 		
 		// Define site_index  PATH (omap-ci)
-		define('SITE_INDEX', $base_url.'index.php/');
+		define('SITE_INDEX', SITE.'index.php/');
 	}
 
 	// --------------------------------------------------------------------
