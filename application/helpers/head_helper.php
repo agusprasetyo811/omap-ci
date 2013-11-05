49116bfc947b88afe2eb72b1af5e0c5dfd8e7720
Agus Prasetyo <agusprasetyo811@gmail.com>
2013-11-06 01:15:41 +0700

    adding head meta helper

--- application/helpers/head_helper.php
+++ application/helpers/head_helper.php
@@ -6,12 +6,20 @@
 
 # Build Javascript script tag
 function js($link) {
+	//$search = array('/\>[^\S ]+/s', '/[^\S ]+\</s','/(\s)+/s');
+	//$replace = array('>','<','\\1');
+	//$temp_body_data = preg_replace($search, $replace, $temp_field);
 	return '<script src="'.$link.'"></script>';
 }
 
 # Build Style script tag
 function style($link) {
 	return '<link href="'.$link.'" rel="stylesheet" type="text/css" />';
+}
+
+# Build meta
+function meta($name, $content) {
+	return '<meta name="'.$name.'" content="'.$content.'/>';
 }
 
 # Build Javascript Link
